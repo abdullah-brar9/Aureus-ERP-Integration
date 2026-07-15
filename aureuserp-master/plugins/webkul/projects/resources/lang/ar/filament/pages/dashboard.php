2dc60bdc02c487c7859329b2dcc5ecde57044344
Abdullah Brar <abdullahahmadbrar9@gmail.com>
2026-07-15 03:12:20 -0700

    Aureus ERP Original
    Original Source code of ERP

---
+++ aureuserp-master/plugins/webkul/projects/resources/lang/ar/filament/pages/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'navigation' => [
+        'title' => 'المشاريع',
+    ],
+
+    'filters-form' => [
+        'project'    => 'المشروع',
+        'assignees'  => 'المُكلَّفون',
+        'tags'       => 'الوسوم',
+        'customer'   => 'العميل',
+        'start-date' => 'تاريخ البداية',
+        'end-date'   => 'تاريخ النهاية',
+    ],
+];
---
+++ aureuserp-master/plugins/webkul/recruitments/resources/lang/ar/filament/pages/recruitment.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'navigation' => [
+        'title' => 'التوظيف',
+    ],
+
+    'filters-form' => [
+        'job-position' => 'المسمى الوظيفي',
+        'departments'  => 'الأقسام',
+        'companies'    => 'الشركات',
+        'stages'       => 'المراحل',
+        'status'       => [
+            'title'   => 'الحالة',
+            'options' => [
+                'all'      => 'الكل',
+                'ongoing'  => 'جاري',
+                'hired'    => 'تم التوظيف',
+                'refused'  => 'مرفوض',
+                'archived' => 'مؤرشف',
+            ],
+        ],
+
+        'start-date' => 'تاريخ البداية',
+        'end-date'   => 'تاريخ النهاية',
+    ],
+];
